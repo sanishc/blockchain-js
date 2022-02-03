8fbd37ec66074bb8d4962d8e4b6570bf90fe27f3
sanish <sanish@pyramidions.co>
2022-02-03 16:51:22 +0530

    [ADD] block mining

--- basic/main.js
+++ basic/main.js
@@ -1,57 +1,67 @@
-import sha1 from "crypto-js/sha1.js";
-
+import SHA256 from "crypto-js/sha256.js";
 class Block {
-  constructor(amount) {
-    (this.amount = amount), (this.hash = ""), (this.prevHash = "");
+  constructor(timestamp, amount, previousHash) {
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.amount = amount;
+    this.hash = this.generateHash();
+    this.nounce = 0;
   }
+
   generateHash() {
-    this.hash = sha1(this.amount + this.hash + this.prevHash).toString();
-    return this.hash;
+    return SHA256(
+      this.prevHash + this.timestamp + this.amount + this.nounce
+    ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nounce += 1;
+      this.hash = this.generateHash();
+    }
+    console.log("Block Mined", this.hash);
   }
 }
 
 class BlockChain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 5;
   }
 
   createGenesisBlock() {
-    const genesisBlock = new Block(0);
-    genesisBlock.generateHash();
-    return genesisBlock;
-  }
-
-  addBlock(newBlock) {
-    newBlock.prevHash = this.getLatestBlock().hash;
-    newBlock.generateHash();
-    this.chain.push(newBlock);
+    return new Block("12-01-2022", 0, "0");
   }
 
   getLatestBlock() {
     return this.chain[this.chain.length - 1];
   }
 
-  hasValidTransactions() {
+  addBlock(newBlock) {
+    newBlock.prevHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
+    this.chain.push(newBlock);
+  }
+
+  validate() {
     for (let i = 1; i < this.chain.length; i++) {
-      const block = this.chain[i];
-      const prevBlock = this.chain[i - 1];
-
-      if (block.hash !== block.generateHash()) {
-        return false;
-      }
-      if (block.prevHash !== prevBlock.hash) {
-        return false;
-      }
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+      if (currentBlock.hash !== currentBlock.generateHash()) return false;
+      if (currentBlock.prevHash !== previousBlock.hash) return false;
     }
-
     return true;
   }
 }
 
 const coin = new BlockChain();
+
+console.log("create block 1");
 coin.addBlock(new Block(10));
+
+console.log("create block 2");
 coin.addBlock(new Block(20));
 
-console.log(coin);
-console.log("\n");
-console.log(coin.hasValidTransactions());
+console.log(coin.chain);
