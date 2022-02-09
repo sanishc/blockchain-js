6c8f773166b32ddd743380a4aa4a66f7d3d537d7
Sanish <sanishc@users.noreply.github.com>
2022-02-09 13:35:19 +0530

    [ADD] Todo APP

---
+++ todo-blockchain/src/constants/index.js
@@ -0,0 +1,4 @@
+import contract from "../contracts/TodoList.json";
+
+export const contractAddress = "0xa87745ACEd464Adb0f3857caA59f134De314Bc94";
+export const abi = contract.abi;
---
+++ todo-blockchain/src/views/TodoInput.jsx
@@ -0,0 +1,33 @@
+import { useRef, useContext } from "react";
+import { EthContext } from "../providers";
+
+const TodoInput = () => {
+  const todoInput = useRef();
+  const { createTodo } = useContext(EthContext);
+
+  const handleClick = () => {
+    createTodo(todoInput.current.value);
+  };
+
+  return (
+    <>
+      <div className="mb-2">
+        <label htmlFor="inputTask" className="form-label">
+          Enter New Task
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="inputTask"
+          placeholder="New Todo..."
+          ref={todoInput}
+        />
+      </div>
+      <button className="btn btn-secondary" onClick={handleClick}>
+        ADD Todo
+      </button>
+    </>
+  );
+};
+
+export default TodoInput;
